90db4eacdb7375f4ffd2c29daa29f2eb3bc8ab97
sayanseksenbaev <sayan123serv@gmail.com>
2025-03-04 23:01:01 +0500

    feat: preparing for new updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
+  },
 })
